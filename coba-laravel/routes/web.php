1eae20e6318d0136b802f62894393519ede51b5d
GoSter223 <alifakbarhilmyauliya2003@gmail.com>
2023-11-12 19:21:48 +0700

    Templating Engine

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -14,11 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "tittle" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "tittle" => "About",
         "name" => "Alif",
         "email" => "alif@gmail.com",
         "image" => "img/a.jpg"
@@ -26,5 +29,53 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            "tittle" => "Judul Post Pertama",
+            "slug" => "Judul-Post-Pertama",
+            "author" => "Alif",
+            "body" => " Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi culpa, excepturi, quas sed suscipit facilis maxime aperiam laboriosam quo fuga quis temporibus expedita fugiat omnis, corporis accusantium rerum a molestiae totam nulla est dolorem et. Doloremque voluptas impedit nesciunt libero eos tempore qui totam quas mollitia praesentium iure porro ex magnam, aliquid voluptates quo sint? Saepe laudantium dolor ullam recusandae sint ad et obcaecati cum consequatur. Fugit tempora earum incidunt. Ut exercitationem, autem maxime cupiditate deserunt ad modi corporis esse quae accusantium in optio architecto quisquam reprehenderit aspernatur natus animi tenetur iste consectetur. Eveniet maiores commodi, doloremque quae aliquam delectus."
+        ],
+        [
+            "tittle" => "Judul Post Kedua",
+            "slug" => "Judul-Post-Kedua",
+            "author" => "Akbar",
+            "body" => " Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi culpa, excepturi, quas sed suscipit facilis maxime aperiam laboriosam quo fuga quis temporibus expedita fugiat omnis, corporis accusantium rerum a molestiae totam nulla est dolorem et. Doloremque voluptas impedit nesciunt libero eos tempore qui totam quas mollitia praesentium iure porro ex magnam, aliquid voluptates quo sint? Saepe laudantium dolor ullam recusandae sint ad et obcaecati cum consequatur. Fugit tempora earum incidunt. Ut exercitationem, autem maxime cupiditate deserunt ad modi corporis esse quae accusantium in optio architecto quisquam reprehenderit aspernatur natus animi tenetur iste consectetur. Eveniet maiores commodi, doloremque quae aliquam delectus."
+        ]
+    ];
+    return view('posts', [
+        "tittle" => "Posts",
+        "posts" => $blog_posts
+    ]);
+});
+
+
+//halaman single post
+Route::get('posts/{slug}', function ($slug) {
+    $blog_posts = [
+        [
+            "tittle" => "Judul Post Pertama",
+            "slug" => "Judul-Post-Pertama",
+            "author" => "Alif",
+            "body" => " Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi culpa, excepturi, quas sed suscipit facilis maxime aperiam laboriosam quo fuga quis temporibus expedita fugiat omnis, corporis accusantium rerum a molestiae totam nulla est dolorem et. Doloremque voluptas impedit nesciunt libero eos tempore qui totam quas mollitia praesentium iure porro ex magnam, aliquid voluptates quo sint? Saepe laudantium dolor ullam recusandae sint ad et obcaecati cum consequatur. Fugit tempora earum incidunt. Ut exercitationem, autem maxime cupiditate deserunt ad modi corporis esse quae accusantium in optio architecto quisquam reprehenderit aspernatur natus animi tenetur iste consectetur. Eveniet maiores commodi, doloremque quae aliquam delectus."
+        ],
+        [
+            "tittle" => "Judul Post Kedua",
+            "slug" => "Judul-Post-Kedua",
+            "author" => "Akbar",
+            "body" => " Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi culpa, excepturi, quas sed suscipit facilis maxime aperiam laboriosam quo fuga quis temporibus expedita fugiat omnis, corporis accusantium rerum a molestiae totam nulla est dolorem et. Doloremque voluptas impedit nesciunt libero eos tempore qui totam quas mollitia praesentium iure porro ex magnam, aliquid voluptates quo sint? Saepe laudantium dolor ullam recusandae sint ad et obcaecati cum consequatur. Fugit tempora earum incidunt. Ut exercitationem, autem maxime cupiditate deserunt ad modi corporis esse quae accusantium in optio architecto quisquam reprehenderit aspernatur natus animi tenetur iste consectetur. Eveniet maiores commodi, doloremque quae aliquam delectus."
+        ]
+    ];
+
+    $new_post = [];
+    foreach ($blog_posts as $post) {
+        if ($post['slug'] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+    return view('post', [
+        "tittle" => "Single post",
+        "post" => $new_post
+    ]);
 });

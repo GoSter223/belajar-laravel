3958c0a1c13ec1840bd1a7eeb3454762ee60dcae
GoSter223 <alifakbarhilmyauliya2003@gmail.com>
2023-11-28 21:28:34 +0700

    redesign UI

--- coba-laravel/resources/views/partials/navbar.blade.php
+++ coba-laravel/resources/views/partials/navbar.blade.php
@@ -6,9 +6,10 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
-          <a class="nav-link {{ ($title === "Home") ? 'active' : '' }}" aria-current="page" href="{{ route('home') }}">Home</a>
-          <a class="nav-link {{ ($title === "About") ? 'active' : '' }}" href="{{ route('about') }}">About</a>
-          <a class="nav-link {{ ($title === "Posts") ? 'active' : '' }}" href="{{ route('posts') }}">Blog</a>
+          <a class="nav-link {{ ($active === "home") ? 'active' : '' }}" aria-current="page" href="{{ route('home') }}">Home</a>
+          <a class="nav-link {{ ($active === "about") ? 'active' : '' }}" href="{{ route('about') }}">About</a>
+          <a class="nav-link {{ ($active === "posts") ? 'active' : '' }}" href="{{ route('posts') }}">Blog</a>
+          <a class="nav-link {{ ($active === "categories") ? 'active' : '' }}" href="{{ route('categories') }}">Categories</a>
         </div>
       </div>
     </div>
--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -19,13 +19,15 @@ use App\Models\User;
 
 Route::get('/', function () {
     return view('home', [
-        "title" => "Home"
+        "title" => "Home",
+        "active" => "home"
     ]);
 })->name('home');
 
 Route::get('/about', function () {
     return view('about', [
         "title" => "About",
+        "active" => "about",
         "name" => "Alif",
         "email" => "alif@gmail.com",
         "image" => "img/a.jpg"
@@ -41,13 +43,15 @@ Route::get('posts/{post:slug}', [PostController::class, 'show'])->name('detail')
 Route::get('/categories', function () {
     return View('categories', [
         'title' => 'Post Categories',
+        'active' => 'categories',
         'categories' => Category::all()
     ]);
-});
+})->name('categories');
 
 Route::get('/categories/{category:slug}', function (Category $category) {
     return View('posts', [
         'title' => "Post By Category : $category->name",
+        'active' => 'categories',
         'posts' => $category->posts->load('category', 'author')
     ]);
 });
